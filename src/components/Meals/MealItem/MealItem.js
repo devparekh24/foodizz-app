606824cc08747f9bd5e9b77638d712ffa6b6599a
devparekh24 <devparekh2424@gmail.com>
2023-12-11 14:37:12 +0530

    added cartItems into Cart using useRef-ref & forwardRef

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './MealItem.css'
 import MealItemForm from './MealItemForm'
+import cartContext from '../../../store/cart-context'
 
 const MealItem = (props) => {
 
     const price = `$${props.price.toFixed(2)}`
+    const cartCtx = useContext(cartContext)
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount,
+            price: props.price
+        })
+    }
     return (
         <li className='meal'>
             <div>
@@ -13,7 +24,7 @@ const MealItem = (props) => {
                 <div className='price'>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     )
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './MealItemForm.css'
 import Input from '../../UI/Input/Input'
 
 const MealItemForm = (props) => {
+
+    const amountInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const enteredAmount = +amountInputRef.current.value;
+        props.onAddToCart(enteredAmount)
+    }
     return (
-        <form className='form'>
-            <Input label='Amount' input={{
-                id: 'amount' + props.id,
-                type: 'number',
-                min: '1',
-                step: '1',
-                defaultValue: '1'
-            }} />
+        <form className='form' onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label='Amount'
+                input={{
+                    id: 'amount' + props.id,
+                    type: 'number',
+                    min: '1',
+                    step: '1',
+                    defaultValue: '1'
+                }} />
             <button>+ Add</button>
         </form>
     )
