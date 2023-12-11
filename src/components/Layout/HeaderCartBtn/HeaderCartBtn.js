30666ea2e70b06431353fae492942070334a6839
devparekh24 <devparekh2424@gmail.com>
2023-12-11 18:34:52 +0530

    making button animated

--- src/components/Layout/HeaderCartBtn/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn/HeaderCartBtn.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartIcon from '../../Cart/CartIcon'
 import cartContext from '../../../store/cart-context'
 import './HeaderCartBtn.css'
@@ -6,12 +6,30 @@ import './HeaderCartBtn.css'
 const HeaderCartBtn = (props) => {
 
     const cartCtx = useContext(cartContext)
+    const [btnAnimated, setBtnAnimated] = useState(false)
+    const { items } = cartCtx
 
-    const numOfCartItems = cartCtx.items.reduce((acc, currentItem) => {
+    useEffect(() => {
+
+        if (items.length === 0) return;
+
+        setBtnAnimated(true)
+
+        const timer = setTimeout(() => {
+            setBtnAnimated(false)
+        }, 100)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+    const numOfCartItems = items.reduce((acc, currentItem) => {
         return acc += currentItem.amount
     }, 0)
+
     return (
-        <button className='button' onClick={props.onCartShow}>
+        <button className={`button ${btnAnimated ? 'bump' : ''}`} onClick={props.onCartShow}>
             <span className='icon'>
                 <CartIcon />
             </span>
